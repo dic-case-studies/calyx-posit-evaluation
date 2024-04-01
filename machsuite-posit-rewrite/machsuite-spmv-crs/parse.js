db58d175c84aaaa52ae1a5af7389f966884a112f
Mark1626 <nimalan.m@protonmail.com>
2024-04-01 15:53:48 +0530

    Nimalan/Mani: refactor: extract common logic of Makefiles

--- machsuite-posit-rewrite/machsuite-spmv-crs/parse.js
+++ machsuite-posit-rewrite/machsuite-spmv-crs/parse.js
@@ -5,7 +5,7 @@ const vals = [
     { field: "cols", width: 32, dim: [1666], input: true, output: false },
     { field: "row_delimiters", width: 32, dim: [495], input: true, output: false },
     { field: "vec", width: 32, dim: [494], input: true, output: false },
-    { field: "out", width: 32, dim: [394], input: false, output: true }
+    { field: "out", width: 32, dim: [494], input: false, output: true }
 ];
 
 transform_machsuite_data_to_json("spmv", vals);
