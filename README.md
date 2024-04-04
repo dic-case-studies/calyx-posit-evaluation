de5038d99d04d4acb1a832fd47f39b50f139aa08
Swethaa <dmagicisinu@gmail.com>
2024-04-04 11:53:12 +0530

    [Maneesh/Swethaa] Update readme and add python dependencies in setup

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Evaluation of Calyx Posit Bindings
 
+## Dependencies
 
+### Tools for hardware development
+
+- Dahlia
+- Calyx
+- Verilator
+- firtool
+
+### Software tools
+
+- node
+- Python
+
+## Instructions to run the benchmarks
+
+- Install python utilities `cd utilities/python; pip3 install -e .; cd ../..`
+- Build cpp utilities `cd utilities/cpp; cmake -B ./build/ -S .; make -C build; cd ../../`
+- For running the benchmarks, do `make`
--- utilities/python/setup.py
+++ utilities/python/setup.py
@@ -2,11 +2,17 @@
 
 from distutils.core import setup
 
-setup(name='posit_calyx_benchmarking',
+setup(
+      name='posit_calyx_benchmarking',
       version='0.0.1',
       description='Scripts validation and plotting',
       author='Nimalan',
       author_email='m.nimalan@thoughtworks.com',
       url='https://github.com/dic-case-studies/calyx-posit-evaluation',
       packages=['validation_utils'],
-      )
+      install_requires=[
+            "simplejson",
+            "seaborn",
+            "scikit-image"
+      ]
+)
